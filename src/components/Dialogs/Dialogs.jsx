9ea0378e19129f8f50e8bb660766c3941f15532b
Den Denowich <denowich@gmail.com>
2024-08-17 23:39:47 +0300

    Lesson - 21_(added in Dialogs name and messages)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,21 @@ import style from './Dialogs.module.css';
 
 export const Dialogs = (props) => {
       return (
-            <div>
-                  <div>Dialogs</div>
+            <div className={style.dialogs}>
+                  <div className={style.dialogItems}>
+                        <div className={style.dialog}>Den</div>
+                        <div className={style.dialog}>Rex</div>
+                        <div className={`${style.dialog} ${style.active}`}>
+                              Danya
+                        </div>
+                        <div className={style.dialog}>Vika</div>
+                        <div className={style.dialog}>Sanya</div>
+                  </div>
+                  <div className={style.messageItems}>
+                        <div className={style.dialog}>Hi!</div>
+                        <div className={style.dialog}>How are you?</div>
+                        <div className={style.dialog}>Yooo!!!</div>
+                  </div>
             </div>
       );
 };

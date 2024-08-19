592bd31970658caa0a732b6be269547aba045f03
Den Denowich <denowich@gmail.com>
2024-08-19 14:07:39 +0300

    Lesson - 22_(added NavLink in Dialogs- name's)

--- src/App.js
+++ src/App.js
@@ -19,22 +19,27 @@ function App() {
                         <div className='app-wrapper-content'>
                               <Routes>
                                     <Route
+                                          exact
                                           path='/profile/*'
                                           element={<Profile />}
                                     ></Route>
                                     <Route
+                                          exact
                                           path='/dialogs/*'
                                           element={<Dialogs />}
                                     ></Route>
                                     <Route
+                                          exact
                                           path='/news/*'
                                           element={<News />}
                                     ></Route>
                                     <Route
+                                          exact
                                           path='/music/*'
                                           element={<Music />}
                                     ></Route>
                                     <Route
+                                          exact
                                           path='/settings/*'
                                           element={<Settings />}
                                     ></Route>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import style from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 export const Dialogs = (props) => {
       return (
             <div className={style.dialogs}>
                   <div className={style.dialogItems}>
-                        <div className={style.dialog}>Den</div>
-                        <div className={style.dialog}>Rex</div>
-                        <div className={`${style.dialog} ${style.active}`}>
-                              Danya
+                        <div className={style.dialog}>
+                              <NavLink to='/dialogs/1'>Den</NavLink>
                         </div>
-                        <div className={style.dialog}>Vika</div>
-                        <div className={style.dialog}>Sanya</div>
+                        <div className={style.dialog}>
+                              <NavLink to='/dialogs/2'>Rex</NavLink>
+                        </div>
+                        <div className={`${style.dialog} ${style.active}`}>
+                              <NavLink to='/dialogs/3'>Danya</NavLink>
+                        </div>
+                        <div className={style.dialog}>
+                              <NavLink to='/dialogs/4'>Vika</NavLink>
+                        </div>
+                        <div className={style.dialog}>
+                              <NavLink to='/dialogs/5'>Sanya</NavLink>
+                        </div>
                   </div>
                   <div className={style.messageItems}>
                         <div className={style.dialog}>Hi!</div>
